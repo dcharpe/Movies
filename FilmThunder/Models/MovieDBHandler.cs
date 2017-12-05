78810336d23750a81bb30020798bfdbe5fe0ff76
dcharpe <n00618091@ospreys.unf.edu>
2017-12-04 21:34:01 -0500

    Commit 2

---
+++ FilmThunder/Models/MovieDBHandler.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+using System.Configuration;
+using System.Data;
+
+namespace FilmThunder.Models
+{
+    public class MovieDBHandler
+    {
+        private SqlConnection con;
+        private void Connection()
+        {
+            string constring = ConfigurationManager.ConnectionStrings["MovieConn"].ToString();
+            con = new SqlConnection(constring);
+        }
+
+        public List<movie> GetAllMovie()
+        {
+            Connection();
+            List<movie> Movielist = new List<movie>();
+
+            SqlCommand cmd = new SqlCommand("displaydatabase", con);
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter sd = new SqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+
+            con.Open();
+            sd.Fill(dt);
+            con.Close();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+
+                Movielist.Add(
+                    new movie
+                    {
+                        filmName = Convert.ToString(dr["Film Name"]),
+                        certName = Convert.ToString(dr["Rating Name"]),
+                        genreName = Convert.ToString(dr["Genre Name"]),
+
+
+                    });
+            }
+            return Movielist;
+        }
+    }
+}
---
+++ FilmThunder/Models/movie.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FilmThunder.Models
+{
+    public class movie
+    {
+        public int filmID { get; set; }
+        public int certID { get; set; }
+        public int genreID { get; set; }
+        public string filmName { get; set; }
+        public string certName { get; set; }
+        public string genreName { get; set; }
+
+        
+    }
+}
